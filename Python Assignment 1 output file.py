de74771eccaf801de7ecafddbfcd1ab83a0056f8
HanselGoyal98 <97552169+HanselGoyal98@users.noreply.github.com>
2022-01-12 00:37:22 +0530

    Add files via upload

---
+++ Python Assignment 1 output file.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/hanse/Desktop/Python Assignment 1.py ============
+Enter the first number: 50
+Enter the second number: 68
+Enter the third number: 45
+The average of the given numbers is  54.333333333333336
+Enter your total income:100000
+Enter your none earning family members:3
+Income tax to be paid is  16200.0
+Enter your SID-21105098
+Enter your name-Hansel Goyal
+Enter your Gender-(M,F,U) -  M
+Enter the course name-ECE
+Enter the CGPA-10
+[21105098, 'Hansel Goyal', 'M', 'ECE', 10.0]
+Enter your marks:92
+Enter your marks:90
+Enter your marks:98
+Enter your marks:96
+Enter your marks:88
+[88.0, 90.0, 92.0, 96.0, 98.0]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple']
---
+++ Python Assignment 1.py
@@ -0,0 +1,64 @@
+# PYTHON ASSIGNMENT 1 
+
+
+#Q1. Average of three numbers 
+
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+num3=float(input("Enter the third number: "))
+
+print("The average of the given numbers is ",(num1+num2+num3)/3)
+
+#Q2. Calculation of income tax 
+
+income=float(input("Enter your total income:"))
+members=int(input("Enter your none earning family members:"))
+
+taxable_income=float(income-10000-members*3000)
+
+income_tax=taxable_income/5
+
+if income_tax<0:
+    print("You don't have to pay income tax.")
+
+else:    
+   print("Income tax to be paid is ", income_tax)
+
+
+#Q3. Storing data in form of list 
+
+SID=int(input("Enter your SID-"))
+name=input("Enter your name-")
+gender=input("Enter your Gender-(M,F,U) -  ")
+course=input("Enter the course name-")
+cgpa=float(input("Enter the CGPA-"))
+
+Student=[SID,name,gender,course,cgpa]
+
+print(Student)
+
+
+#Q4. Sorting marks of 5 students 
+ 
+A1=float(input("Enter your marks:"))                #A1 is marks of student 1
+A2=float(input("Enter your marks:"))                #A2 is marks of student 2
+A3=float(input("Enter your marks:"))                #A3 is marks of student 3
+A4=float(input("Enter your marks:"))                #A4 is marks of student 4
+A5=float(input("Enter your marks:"))                #A5 is marks of student 5
+
+Marks=[A1,A2,A3,A4,A5]
+Marks.sort()
+print(Marks)
+
+
+#Q5.1 Removing certain element from a list 
+
+
+colour=['Red','Green','White','Black','Pink','Yellow']
+colour.remove('Black')
+print(colour)
+
+#Q5.2 Overwriting elements of a list 
+
+colour[3:5]=["Purple"]
+print(colour)
