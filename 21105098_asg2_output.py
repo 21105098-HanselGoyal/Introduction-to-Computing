697715fcf4553f6d2645ac3613e9ea34e74571bb
21105098_Hansel Goyal <97552169+21105098-HanselGoyal@users.noreply.github.com>
2022-01-21 23:55:07 +0530

    Add files via upload

---
+++ 21105098_asg2_output.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:\Users\hanse\Desktop\21105098_asgn_2.py ==============
+
+Part a 
+Length of Input String = 35
+
+Part b
+egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive
+
+Part d
+Python is object oriented language
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+
+
+ Hey,Hansel Goyal here!
+ My SID is 21105098
+ I am from ECE and my CGPA is 10
+
+
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 0
+
+
+Enter 1st no. : 20
+Enter 2nd no. : 50
+Enter 3rd no. : 60
+Highest no. = 60
+
+
+Enter input string: Hansel
+No
+
+
+Enter 1st length : 20
+Enter 2nd length : 25
+Enter 3rd length : 30
+Yes
