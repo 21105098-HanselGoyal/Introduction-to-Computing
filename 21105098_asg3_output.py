0a735dbbfc1349a9bf05db86a6ed5f953cdd11f2
21105098_Hansel Goyal <97552169+21105098-HanselGoyal@users.noreply.github.com>
2022-02-11 14:22:58 +0530

    Add files via upload

---
+++ 21105098_asg3_output.py
@@ -0,0 +1,102 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:\Users\hanse\Desktop\python assignment.py =============
+
+Q.1
+Enter the string:My name is Hansel Goyal
+
+Dictionary containing {'Word':'Word Count'} is shown below:
+{'goyal': 1, 'is': 1, 'my': 1, 'name': 1, 'hansel': 1}
+
+Q.2
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:27
+Enter Month [1-12]:04
+Enter Year [1800-2025]:2004
+
+Next Date in format Day/Month/Year is: 28/4/2004
+
+Q.3
+Enter the numbers separated by space:4
+
+List containing (n,n^2) is shown below:
+[(4, 16)]
+
+Q.4
+Enter grade points:10
+Your grade is 'A+' and Outstanding performance.
+
+Q.5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Q.6
+Enter student name:Hansel Goyal
+Enter student SID:21105098
+Enter Y to continue or N to end:Y
+Enter student name:Hari Om
+Enter student SID:123456789
+Enter Y to continue or N to end:N
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105098: 'Hansel Goyal', 123456789: 'Hari Om'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{21105098: 'Hansel Goyal', 123456789: 'Hari Om'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{21105098: 'Hansel Goyal', 123456789: 'Hari Om'}
+
+Q.6(d)
+Enter SID to get name of student:21105098
+Name of student with SID 21105098 is Hansel Goyal.
+
+Q.7
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=3
+
+The fibonacci series with 3 elements is shown below:
+[1, 1, 2]
+
+Average of given fibonacci series is 1.33
+
+Q.8
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
